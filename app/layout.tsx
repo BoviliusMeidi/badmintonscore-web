23c4f83ee38282e740718f762a0e506d40cb9587
BoviliusMeidi <boviliusbm@gmail.com>
2025-11-02 01:15:20 +0700

    feat: add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Russo_One, Tinos } from "next/font/google";
 import "./globals.css";
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body className={`${russoOne.variable} ${tinos.variable} antialiased`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
