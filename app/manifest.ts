d23f1a5100baaff25099adab97ffa1b7178ad083
BoviliusMeidi <boviliusbm@gmail.com>
2026-04-19 21:23:54 +0700

    feat: enhance manifest with screenshots for mobile and desktop views

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,26 +1,43 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Badminton Score',
-    short_name: 'Badminton Score',
-    description: 'A modern badminton scoreboard website for singles & doubles. Accurately implements BWF serving/receiving rules, tracks score, sets, and match time. Features include live stats, point-by-point history, undo, and a detailed match summary.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#ffffff',
-    orientation: 'portrait',
+    id: "/",
+    name: "Badminton Score",
+    short_name: "Badminton Score",
+    description:
+      "A modern badminton scoreboard website for singles & doubles. Accurately implements BWF serving/receiving rules, tracks score, sets, and match time. Features include live stats, point-by-point history, undo, and a detailed match summary.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
+    orientation: "portrait",
     icons: [
       {
-        src: '/192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/192x192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
-  }
+    screenshots: [
+      {
+        src: "/screenshots/mobile-view.png",
+        sizes: "1080x1920",
+        type: "image/png",
+        label: "Badminton Score Mobile View",
+      },
+      {
+        src: "/screenshots/desktop-view.png",
+        sizes: "1920x1080",
+        type: "image/png",
+        label: "Badminton Score Desktop View",
+        form_factor: "wide",
+      },
+    ],
+  };
 }
